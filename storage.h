83866dafa40751a7b6a05c81064073abee969122
Beginner10617 <wasihusain23@iitk.ac.in>
2026-08-08 18:30:49 +0530

    changed pointer members to array members in user cred

--- storage.h
+++ storage.h
@@ -3,10 +3,14 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#define MAX_PUBLIC_KEY_SIZE 1024
+#define MAX_PASSWORD_HASH_SIZE 256
+#define MAX_SALT_SIZE 8
 
 typedef struct {
   const char *username;
-  const uint8_t *password_hash, *salt;
+  const uint8_t public_key[MAX_PUBLIC_KEY_SIZE],
+      password_hash[MAX_PASSWORD_HASH_SIZE], salt[8];
 } user_cred;
 
 typedef struct {
